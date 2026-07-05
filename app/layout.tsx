c04e9b4ffd3a55c13496288afcdb9b7ddb7ccb94
Nabin Dhakal <nbn@Nabins-MacBook-Pro.local>
2026-07-05 17:06:36 +1000

    Add AdSense verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" data-scroll-behavior="smooth">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4957639132748492"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={`${inter.variable} ${sora.variable}`}>
         <Layout>{children}</Layout>
       </body>
